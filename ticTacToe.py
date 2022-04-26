bae951b5a81743d78f3ffe86a836637abc760c5d
Joseph <per16068@byui.edu>
2022-04-25 20:57:42 -0400

    Separated the code into two functions, one for validation one for modifiying the string

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,15 +1,30 @@
+import re
 grid = "1|2|3\n-+-+-\n4|5|6\n-+-+-\n7|8|9"
-def body(ans,entity):
-    grid.replace(str(ans),entity)
-    print(grid)
+def game_logic(ans,entity):
+    gr = grid.replace(str(ans),entity)
+    return gr
+def validation(ans,entity):
+    global grid
+    gri = re.sub("\n\-\+\-\+\-\n","|",grid)
+    gri = gri.split("|")
+    while ans not in range(1,9):
+        print("You Must select a different number")
+        ans = int(input(f"{entity}'s turn to choose a square (1-9): "))
+    while gri[ans-1] =="x" or gri[ans-1] == "O":
+        print("You Must select a different number")
+        ans = int(input(f"{entity}'s turn to choose a square (1-9): "))
+    result = game_logic(ans,entity)
+    return result
 def main():
-
     bo = True
+    global grid
     print(grid)
     while bo != False:
         x = int(input("x's turn to choose a square (1-9): "))
-        grid  = body(x,"x")
+        grid  = validation(x,"x")
+        print(grid)
         O = int(input("O's turn to choose a square (1-9): "))
-        grid = body(O,"O")
+        grid = validation(O,"O")
+        print(grid)
 main()
 
